a1461bdd2fb36789ea950999242316a1f924321f
Chris Powell <chaoscode@gmail.com>
2024-07-25 12:19:05 -0400

    Added comments. Had the orginal prompted passed alone with the the data from wiki.

--- wikipedia_pipeline.py
+++ wikipedia_pipeline.py
@@ -1,6 +1,8 @@
 from typing import List, Union, Generator, Iterator
 from pydantic import BaseModel
 from schemas import OpenAIChatMessage
+# This is a modified copy of URL: https://github.com/open-webui/pipelines/blob/main/examples/pipelines/integrations/wikipedia_pipeline.py
+
 import requests
 import os
 
@@ -36,32 +38,41 @@ class Pipeline:
         # This is where you can add your custom pipelines like RAG.
         print(f"pipe:{__name__}")
 
+        # Could add commands to configure the pipeline here
         if body.get("title", False):
             print("Title Generation")
+            return "Wikipedia Pipeline"
+        elif body.get("help", False):
             return "Wikipedia Pipeline"
         else:
             titles = []
             for query in [user_message]:
                 # We should escape encode this not replace with _
                 query = query.encode('unicode-escape')
-               
-                # query = query.replace(" ", "_")
-
+            
+                # Pull all titles for the topic limited to 1 result
                 r = requests.get(
                     f"https://en.wikipedia.org/w/api.php?action=opensearch&search={query}&limit=1&namespace=0&format=json"
                 )
 
+                # Get response
                 response = r.json()
                 titles = titles + response[1]
+                
+                # Print out all the titles
                 print(titles)
 
-            context = None
+            # Setup the context that goes to the LLM
+            context = user_message + "\n"
+            
+            # If we have titles from above, get context data from wiki in json
             if len(titles) > 0:
                 r = requests.get(
                     f"https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles={'|'.join(titles)}"
                 )
                 response = r.json()
-                # get extracts
+                
+                # get extracts and add them to the LLM context
                 pages = response["query"]["pages"]
                 for page in pages:
                     if context == None:
@@ -69,4 +80,5 @@ class Pipeline:
                     else:
                         context = context + pages[page]["extract"] + "\n"
 
-            return context if context else "No information found"
+            # Return new context from wiki
+            return context if context else "No information found from wiki"
